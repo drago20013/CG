a92cf138a2b1146a0e619bd8583e7be1d2dc1d53
Michal <smaluchmichal@gmail.com>
2023-04-12 10:38:59 +0200

    init

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameObject camHolder;
+    public float speed, sensitivity, maxForce, jumpForce;
+    private Vector2 move, look;
+    private float lookRotation;
+    public bool grounded;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        look = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Jump();    
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        Look();
+    }
+
+    void Move()
+    {
+        Vector3 currentVelocity = rb.velocity;
+        Vector3 targetVelocity = new Vector3(move.x, 0, move.y);
+        targetVelocity *= speed;
+
+        targetVelocity = transform.TransformDirection(targetVelocity);
+
+        Vector3 velocityChange = (targetVelocity - currentVelocity);
+        velocityChange = new Vector3(velocityChange.x, 0f, velocityChange.z);
+
+        Vector3.ClampMagnitude(velocityChange, maxForce);
+
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+
+    void Look()
+    {
+        transform.Rotate(Vector3.up * look.x * sensitivity);
+        //lookRotation += (-look.y * sensitivity);
+
+        //lookRotation = Mathf.Clamp(lookRotation, -90, 90);
+        camHolder.transform.eulerAngles = new Vector3(0f, camHolder.transform.eulerAngles.y, camHolder.transform.eulerAngles.z);
+    }
+
+    void Jump()
+    {
+        Vector3 jumpForces = Vector3.zero;
+
+        if (grounded)
+        {
+            jumpForces = Vector3.up * jumpForce;
+        }
+
+        rb.AddForce(jumpForces, ForceMode.VelocityChange);
+    }
+
+    public void SetGrounded(bool state)
+    {
+        grounded = state;
+    }
+}
