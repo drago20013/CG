a1d41b7f809a0bb08474d9ac4d5894f7f4e8b11b
claude-perrin <vity5.diduk@gmail.com>
2023-05-07 23:01:53 +0200

    potions toggling

---
+++ Assets/Scripts/PotionToDisplay.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class PotionToDisplay : MonoBehaviour
+{
+    public Image currentPotionImage;
+
+    // Technically sprites should be taken in smarter way, but who fucking cares
+    public Sprite antiGravityImage;
+    public Sprite increaseWeightImage;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // when no potion make it false always
+        currentPotionImage.enabled = true;
+    }
+
+    // please forgive me god, it is the ugliest flag but I want to sleep today
+    public void changeImage(string potionName) 
+    {  
+        if (potionName == "antiGravity")
+        {
+            currentPotionImage.sprite = antiGravityImage;
+        }
+
+        else if (potionName == "increaseWeight")
+        {
+            currentPotionImage.sprite = increaseWeightImage;
+        }
+        else if (potionName == "null") 
+        {
+            currentPotionImage.enabled = false;
+        }
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
