c559639f08a87132fda74d773e70db1c8adca8f1
claude-perrin <vity5.diduk@gmail.com>
2023-06-05 17:28:00 +0200

    level 2 completed

--- Assets/Scripts/SpongeOnWaterTrigger.cs
+++ Assets/Scripts/SpongeOnWaterTrigger.cs
@@ -8,16 +8,19 @@ public class SpongeOnWaterTrigger : MonoBehaviour
 {
     //public UIController uiController;
     public GameObject activateIcon;
-    //public boolean SpongeOnWater = false;
+    public Rigidbody rb;
+    // public boolean SpongeOnWater = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.layer == 4) {
-            SpongeController controller = gameObject.GetComponent<SpongeController>();
-            if (controller.enabled == false) {
-                activateIcon.SetActive(true);
-                }
-            
+        SpongeController controller = gameObject.GetComponent<SpongeController>();
+        if(other.gameObject.layer == 4 && controller.enabled == false) {
+            activateIcon.SetActive(true);
+        }
+        if(other.gameObject.layer == 7) {
+            rb.velocity = new Vector3(0f,0f,0f); 
+            rb.angularVelocity = new Vector3(0f,0f,0f);
+            rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
         }
     }
 }
